68ba9b745b1cdaffdf017561bf14f7d91583b8b9
thanaratth63 <108852188+thanaratth63@users.noreply.github.com>
2022-08-25 00:06:47 +0700

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,5 @@
 let express = require('express')
-let bodyParser = require('body-parser')
-const {sequelize} = require('./models')
-
 const app = express()
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-
-require('./routes')(app)
 
 app.get('/status', function (req, res ){
   res.send('Hello nodejs server')
@@ -21,8 +13,6 @@ app.get('/hello/:person', function (req,res) {
 
 let port = 8081
 
-sequelize.sync({force: false}).then(() => {
-  app.listen(port, function () {
-      console.log('Server running on ' + port)
-  })
+app.listen(port,function(){
+  console.log('Server running on ' + port)
 })
